2c0c1e2b8b7c07e24172c3421ba250b137b10c14
Austin Ezell <austin.r.ezell@gmail.com>
2015-08-29 09:04:29 -0700

    Committment scares me

---
+++ main.js
@@ -0,0 +1,111 @@
+'use strict';
+
+$(document).ready(init);
+function init(){
+  var currentVal = "";
+  var runningVal = null
+  var operand = null;
+  // $('.btn').click(clearError);
+  $('.number').click(onNumber);
+  $('.operand').click(onOperand);
+  $('#clear').click(clear);
+  $('#clear').dblclick(totalClear)
+  $('#negative').click(onNegative);
+  $('#percentage').click(onPercentage);
+  $('#equals').click(onOperand);
+
+  function onNumber(){
+    if(operand === 'equals' || runningVal === 'error'){
+      runningVal = null;
+    }
+    var val = $(this).data('id');
+    currentVal += val.toString();
+    changeDisplay(currentVal);
+
+  }
+
+  function onOperand(){
+    if (currentVal !== ''){
+      var cVal = parseFloat(currentVal);
+      if (runningVal !== null){
+        switch (operand) {
+          case 'divide':
+          if (cVal){
+            runningVal /= cVal;
+          }
+          else{
+            runningVal = 'error';
+            currentVal = ''
+            operand = null
+            break;
+          }
+          break;
+          case 'multiply':
+          runningVal *= cVal;
+          break;
+          case 'subtract':
+          runningVal -= cVal;
+          break;
+          case 'addition':
+          runningVal += cVal;
+          break;
+        }
+        changeDisplay(runningVal);
+      }
+      else{
+        runningVal = cVal;
+      }
+    };
+    operand = $(this).data('id');
+    currentVal = '';
+  }
+
+  function clear(){
+    currentVal = '';
+    changeDisplay('0');
+  }
+
+  function onNegative(){
+    var negativeVal = parseFloat(getHolderVal())*(-1);
+    if (currentVal !== ''){
+      currentVal = negativeVal;
+    }else {
+      runningVal = negativeVal;
+    }
+    changeDisplay(negativeVal)
+  }
+
+  function onPercentage(){
+    var percentageVal = parseFloat(getHolderVal())/100;
+    if (currentVal !== ''){
+      currentVal = percentageVal;
+    }else {
+      runningVal = percentageVal;
+    }
+    changeDisplay(percentageVal);
+  }
+
+  function getHolderVal(){
+    return $('#currentVal').text()
+  }
+
+  function totalClear(){
+    currentVal = "";
+    runningVal = null
+    operand = null;
+    changeDisplay('0')
+  }
+
+  // function clearError(){
+  //   if (runningVal === 'error'){
+  //     console.log('i work, bitch');
+  //     var currentVal = "";
+  //     var runningVal = null
+  //     var operand = null;
+  //   }
+  // }
+
+  function changeDisplay(value){
+    $('#currentVal').text(value);
+  }
+};
